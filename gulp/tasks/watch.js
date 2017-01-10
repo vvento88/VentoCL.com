3aaa6b1cb4482c6596fa93cbbbc9d996141e756a
Victor <victor.vento@gmail.com>
2017-01-10 07:44:56 -0700

    Completed automation Gulp tasks for workflow

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browserSync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir:'app'
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject',['styles'], function() {
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
+});
