b32acc86e393beb8d42bd541f05036025571a377
Victor <victor.vento@gmail.com>
2017-01-18 10:38:41 -0700

    began JS for mobile menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,9 +18,17 @@ gulp.task('watch', function() {
   watch('./app/assets/style/**/*.css', function() {
     gulp.start('cssInject');
   });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  });
 });
 
 gulp.task('cssInject',['styles'], function() {
   return gulp.src('./app/temp/style/style.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
